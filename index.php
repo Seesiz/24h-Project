4857ef21b8988f4d0c167e5a2f6f36ecc6ccc621
Seesiz <108148026+Seesiz@users.noreply.github.com>
2022-12-15 17:47:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/bootstrap-5.2.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="asset/bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/bootstrap-5.2.2-dist/css/bootstrap.css.map">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Airbnb</title>
+</head>
+<!-- Navigation -->
+    <?php 
+        include('inc/header.php');
+    ?>
+<body>
+    
+</body>
+<meta charset="UTF-8">
+<script src="asset/bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js"></script>
+</html>
