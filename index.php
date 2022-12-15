a1c81c6354fef22b387b6d3c2cc396f42a550cc7
Seesiz <108148026+Seesiz@users.noreply.github.com>
2022-12-15 22:13:50 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
+<?php
+    session_start();
+    $page = "acceuil";
+    if(isset($_GET["page"])){
+        $page = $_GET["page"];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/bootstrap-5.2.2-dist/css/bootstrap.css">
@@ -10,12 +18,22 @@
     <title>Airbnb</title>
 </head>
 <!-- Navigation -->
+<header>
     <?php 
         include('inc/header.php');
     ?>
+</header>
 <body>
-    
+    <?php 
+        if($page=="incription"){
+            include('inc/Inscription.php');
+        }if($page=="connexion"){
+            include('inc/Connexion.php');
+        }if($page=="acceuil"){
+            include('inc/acceuil.php');
+        }
+    ?>
+        
 </body>
-<meta charset="UTF-8">
 <script src="asset/bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js"></script>
 </html>
